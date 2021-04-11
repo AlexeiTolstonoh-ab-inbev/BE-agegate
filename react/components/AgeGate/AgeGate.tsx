50b1353f73a7fa44c3ac5e0a145310cdf591c6c1
AlexeiTolstonog-ab-inbev <Alexei.Tolstonoh@ab-inbev.com>
2021-04-11 11:38:47 +0300

    add avtofocus on inputs

--- react/components/AgeGate/AgeGate.tsx
+++ react/components/AgeGate/AgeGate.tsx
@@ -5,7 +5,10 @@ import styles from './AgeGate.css'
 
 const AgeGate = () => {
 
-
+	const dayInput: any = React.createRef();
+	const mounthInput: any = React.createRef();
+	const yearInput: any = React.createRef();
+	const rememberMeCheckbox: any = React.createRef();
 	const [cookies, setCookie] = useCookies(['BEageGate'])
 	const [day, setDay] = useState(`0`)
 	const [mounth, setMounth] = useState(`0`)
@@ -16,8 +19,8 @@ const AgeGate = () => {
 
 	useEffect(() => {
 		const lang =window.location.href
-		const a =lang.substr(-3,2)
-		setLanguage(a)
+		const path =lang.substr(-3,2)
+		setLanguage(path)
 	}, [])
 
 	const handleClick = () => {
@@ -33,7 +36,8 @@ const AgeGate = () => {
 	const onClickConfirm = () => {
 		const msIn18Years = 568025136000
 		const nowDate = Date.now()
- 		const inputDateInMs = Date.parse(`${year}-${mounth}-${day}`)
+ 		const inputDateInMs = Date.parse(`${Number(year)}-${Number(mounth)}-${Number(day)}`)
+		debugger
 		const isLegal = nowDate - inputDateInMs >= msIn18Years ? true : false
 		if(!isLegal) {
 			setAccess(false)
@@ -48,7 +52,45 @@ const AgeGate = () => {
 
 	}, [])
 
-
+	const changeDay =(e: any)=>{
+		let day = e.target.value
+		if(day < 1){
+			day = 1
+		}
+		else if(day > 31){
+			day = 31
+		}
+		setDay(day)
+		if(day.length > 1){
+			mounthInput.current.focus()
+		}
+	}
+	const changeMounth =(e: any)=>{
+		let mounth = e.target.value
+		if(mounth < 1){
+			mounth = 1
+		}
+		else if(mounth > 12){
+			mounth = 12
+		}
+		setMounth(mounth)
+		if(mounth.length > 1){
+			yearInput.current.focus()
+		}
+	}
+	const changeYear =(e: any)=>{
+		let year = e.target.value
+		if(year < 1900){
+			year = 1900
+		}
+		else if(year > 2020){
+			year = 2020
+		}
+		setYear(year)
+		if(year.length > 3){
+			rememberMeCheckbox.current.focus()
+		}
+	}
 	return (
 		<div>
 			{cookies.BEageGate == 'false' && langueage !== '' &&(
@@ -61,14 +103,14 @@ const AgeGate = () => {
 							<p className={styles.logoText}>{langueage === 'en' ? `Are you over Legal Drinking Age?` : langueage === 'fr' ? `Avez-vous dépassé l'âge légal pour boire?` : `Bent u ouder dan de wettelijke drinkleeftijd?`}</p>
 								<p  className={styles.ageQuestion}>{langueage === 'en' ? `Please enter your date of birth` : langueage === 'fr' ? 'Veuillez entrer votre date de naissance' : 'Voer je geboortedatum in'}</p>
 							<div className={styles.inputContainer}>
-							<input className={styles.dataInput} onChange={e => setDay(e.target.value)} autoFocus min={1} max={31} type="number" name="agegate-d" maxLength={2} placeholder="dd" tabIndex={0}></input>
-							<input className={styles.dataInput} onChange={e => setMounth(e.target.value)} min={1} max={12} type="number" name="agegate-m" maxLength={2} placeholder="mm" tabIndex={0}></input>
-							<input className={styles.dataInput} onChange={e => setYear(e.target.value)} min={0} type="number" name="agegate-y" maxLength={4} placeholder="yyyy" tabIndex={0}></input>
+							<input ref={dayInput} className={styles.dataInput} onChange={e =>  changeDay(e)} autoFocus  type="text" name="agegate-d" maxLength={2} placeholder="dd" tabIndex={0}></input>
+							<input ref={mounthInput} className={styles.dataInput} onChange={e => changeMounth(e)} type="text" name="agegate-m" maxLength={2} placeholder="mm" tabIndex={0}></input>
+							<input ref={yearInput} className={styles.dataInput} onChange={e => changeYear(e)}type="text" name="agegate-y" maxLength={4} placeholder="yyyy" tabIndex={0}></input>
 						</div>
 							<input className={styles.checkboxRemember} id="remember" type="checkbox" onChange={()=> setRemember(!isRemember)}></input>
 							<label htmlFor={'remember'} className={styles.ageLable}>{langueage === 'en' ? `Remember me` : langueage === 'fr' ? 'Souviens-toi de moi' : 'Onthoud mij'}</label>
 
-							 <button className={styles.ageGateButton} onClick={onClickConfirm}>
+							 <button ref={rememberMeCheckbox} className={styles.ageGateButton} onClick={onClickConfirm}>
 								{langueage === 'en' ? `CONTINUE` : langueage === 'fr' ? 'CONTINUER' : 'DOORGAAN MET'}
 						</button>
 						</div> : <div><p className={styles.unswer}>{langueage === 'en' ? `Sorry, you must be 18 years or older to visit this website` : langueage === 'fr' ? 'Désolé, vous devez être âgé d`au moins 18 ans pour visiter ce site Web' : 'Sorry, je moet 18 jaar of ouder zijn om deze website te bezoeken'}</p></div>}
