2d34fe5b9a0cf421a6e9610a4151add60e61caec
AlexeiTolstonog-ab-inbev <Alexei.Tolstonoh@ab-inbev.com>
2021-06-29 12:06:41 +0300

    update validation

--- react/components/AgeGate/AgeGate.tsx
+++ react/components/AgeGate/AgeGate.tsx
@@ -45,9 +45,9 @@ const AgeGate = () => {
 		const currentMounth = Number(mounth)<10? `0${Number(mounth)}`: Number(mounth)
 		const currentDay = Number(day)<10? `0${Number(day)}`: Number(day)
 		const dateString = `${Number(year)}-${currentMounth}-${currentDay}T24:00:00`
- 		const inputDateInMs = Date.parse(dateString)
+		const inputDateInMs = Date.parse(dateString)
 		const ageInMs = nowDate - inputDateInMs
-console.log(ageInMs)
+		console.log(ageInMs)
 		if(ageInMs < msIn18Years) {
 			console.log('no')
 			setAccess(false)
@@ -100,38 +100,44 @@ console.log(ageInMs)
 
 		if(year.length > 3){
 			if(year < 1900){
-			year = 1900
-		}
-		else if(year > 2020){
-			year = 2020
-		}
+				year = 1900
+			}
+			else if(year > 2020){
+				year = 2020
+			}
 			yearInput.current.value = year
 			setYear(year)
 			rememberMeCheckbox.current.focus()
 		}
 	}
+	const submitHandler =(evt:any) =>{
+		evt.preventDefault()
+	}
+
 	return (
 		<div>
 			{cookies.BEageGate == 'false' && langueage !== '' &&(
- 				<div className={styles.ageGateWrapper}>
+				<div className={styles.ageGateWrapper}>
 					<div className={styles.ageGateContainer}>
 						<div className={`${styles.ageGateLogoContainer} flex`}>
-								<img className={styles.logoImage} src="https://abibewebshop.vtexassets.com/assets/vtex.file-manager-graphql/images/b51b748e-9c50-4248-9b51-68846a2d8359___014546075148f2f1404a41002ba78895.png"></img>
+							<img className={styles.logoImage} src="https://abibewebshop.vtexassets.com/assets/vtex.file-manager-graphql/images/b51b748e-9c50-4248-9b51-68846a2d8359___014546075148f2f1404a41002ba78895.png"></img>
 						</div>
 						{isAccess ? <div className={styles.ageGateUnderAge}>
 							<p className={styles.logoText}>{langueage === 'en' ? `Are you over Legal Drinking Age?` : langueage === 'fr' ? `ÊTES-VOUS EN ÂGE LÉGAL DE BOIRE DE L'ALCOOL ?` : `Bent u ouder dan de wettelijke drinkleeftijd?`}</p>
-								<p  className={styles.ageQuestion}>{langueage === 'en' ? `Please enter your date of birth` : langueage === 'fr' ? 'Veuillez entrer votre date de naissance' : 'Bent u 18 jaar of ouder ?'}</p>
-							<div className={styles.inputContainer}>
-							<input ref={dayInput} className={styles.dataInput} onChange={e =>  changeDay(e)} autoFocus  type="text" name="agegate-d" maxLength={2} placeholder={langueage === 'fr'? 'jj' : 'dd'} tabIndex={0}></input>
-							<input ref={mounthInput} className={styles.dataInput} onChange={e => changeMounth(e)} type="text" name="agegate-m" maxLength={2} placeholder="mm" tabIndex={0}></input>
-							<input ref={yearInput} className={styles.dataInput} onChange={e => changeYear(e)}type="text" name="agegate-y" maxLength={4} placeholder={langueage === 'fr'? 'aaaa' : langueage === 'nl'? 'jjjj' : 'yyyy'} tabIndex={0}></input>
-						</div>
-							<input className={styles.checkboxRemember} id="remember" type="checkbox" onChange={()=> setRemember(!isRemember)}></input>
-							<label htmlFor={'remember'} className={styles.ageLable}>{langueage === 'en' ? `Remember me` : langueage === 'fr' ? 'Se souvenir de moi' : 'Onthoud mij'}</label>
+							<p  className={styles.ageQuestion}>{langueage === 'en' ? `Please enter your date of birth` : langueage === 'fr' ? 'Veuillez entrer votre date de naissance' : 'Bent u 18 jaar of ouder ?'}</p>
+							<form onSubmit={(evt)=>submitHandler(evt)} className={styles.ageGateUnderAge}>
+								<div className={styles.inputContainer}>
+									<input ref={dayInput} className={styles.dataInput} onChange={e =>  changeDay(e)} autoFocus  type="text" name="agegate-d" required={true} pattern={`^[ 0-9]+$`} minLength={2} maxLength={2} placeholder={langueage === 'fr'? 'jj' : 'dd'} tabIndex={0}></input>
+									<input ref={mounthInput} className={styles.dataInput} onChange={e => changeMounth(e)} type="text" name="agegate-m" required={true}  pattern={`^[ 0-9]+$`} minLength={2} maxLength={2} placeholder="mm" tabIndex={0}></input>
+									<input ref={yearInput} className={styles.dataInput} onChange={e => changeYear(e)}type="text" name="agegate-y" required={true}  pattern={`^[ 0-9]+$`} minLength={4} maxLength={4} placeholder={langueage === 'fr'? 'aaaa' : langueage === 'nl'? 'jjjj' : 'yyyy'} tabIndex={0}></input>
+								</div>
+								<input className={styles.checkboxRemember} id="remember" type="checkbox" onChange={()=> setRemember(!isRemember)}></input>
+								<label htmlFor={'remember'} className={styles.ageLable}>{langueage === 'en' ? `Remember me` : langueage === 'fr' ? 'Se souvenir de moi' : 'Onthoud mij'}</label>
 
-							 <button ref={rememberMeCheckbox} className={styles.ageGateButton} onClick={onClickConfirm}>
-								{langueage === 'en' ? `CONTINUE` : langueage === 'fr' ? 'CONTINUER' : 'DOORGAAN'}
-						</button>
+								<button ref={rememberMeCheckbox} className={styles.ageGateButton} onClick={onClickConfirm}>
+									{langueage === 'en' ? `CONTINUE` : langueage === 'fr' ? 'CONTINUER' : 'DOORGAAN'}
+								</button>
+							</form>
 						</div> : <div><p className={styles.unswer}>{langueage === 'en' ? `Sorry, you must be 18 years or older to visit this website` : langueage === 'fr' ? 'Désolé, vous devez être âgé d`au moins 18 ans pour visiter ce site Web' : 'Sorry, je moet 18 jaar of ouder zijn om deze website te bezoeken'}</p></div>}
 						<div className={styles.textContainer}>
 							<p className={styles.descriptionBottomText}>{langueage === 'en' ? `Our craftmanship is enjoyed responsibly.` : langueage === 'fr' ? 'Notre savoir-faire se déguste avec sagesse.' : 'Ons vakmanschap drink je met verstand.'}</p>
